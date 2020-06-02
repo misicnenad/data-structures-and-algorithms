08b3fa896b400686a4d82ee47af4088d90fde66c
shonmisic <19427053+shonmisic@users.noreply.github.com>
2020-06-02 22:18:56 +0200

    Add exercises

---
+++ DataStructuresAndAlgorithms/BigO/ExerciseBigOCalculation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace DataStructuresAndAlgorithms.BigO
+{
+    public class ExerciseBigOCalculation
+    {
+        public void Run()
+        {
+            var input = Enumerable.Repeat(new object(), 100).ToArray();
+
+            FunChallenge(input);
+        }
+
+        // Big O = O(3 + 4n) = O(n)
+        private int FunChallenge(object[] input)
+        {
+            var a = 10; // O(1)
+            a = 50 + 3; // O(1)
+
+            for (int i = 0; i < input.Length; i++)  // O(n)
+            {
+                AnotherFunction(); // O(n)
+                var stranger = true; // O(n)
+                a++; // O(n)
+            }
+
+            return a; // O(1)
+        }
+
+        private void AnotherFunction()
+        {
+            Console.WriteLine("Another Function");
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/BigO/ExerciseBigOCalculation2.cs
@@ -0,0 +1,35 @@
+﻿namespace DataStructuresAndAlgorithms.BigO
+{
+    public class ExerciseBigOCalculation2
+    {
+        public void Run()
+        {
+            var input = 100;
+
+            AnotherFunChallenge(input);
+        }
+
+        // Big O = O(4 + 5n) = O(n)
+        private void AnotherFunChallenge(int input)
+        {
+            var a = 5; // O(1)
+            var b = 10; // O(1)
+            var c = 50; // O(1)
+
+            for (int i = 0; i < input; i++)
+            {
+                var x = i + 1; // O(n)
+                var y = i + 2; // O(n)
+                var z = i + 3; // O(n)
+            }
+
+            for (int j = 0; j < input; j++)
+            {
+                var p = j * 2; // O(n)
+                var q = j * 2; // O(n)
+            }
+
+            var whoAmI = "I don't know"; // O(1)
+        }
+    }
+}
