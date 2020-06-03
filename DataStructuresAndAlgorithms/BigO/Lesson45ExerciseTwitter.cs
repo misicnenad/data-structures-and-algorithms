1d5864b8c57a31cc4fea97e98be3f60890f3265a
shonmisic <19427053+shonmisic@users.noreply.github.com>
2020-06-03 08:09:04 +0200

    Add lesson 45

---
+++ DataStructuresAndAlgorithms/BigO/Lesson45ExerciseTwitter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithms.BigO
+{
+    public class Lesson45ExerciseTwitter
+    {
+        // Find 1st, Find Nth..
+        public void Run()
+        {
+            var array = new string[] { "hi", "my", "teddy" };
+
+            Console.WriteLine(array[0]); // O(1)
+            Console.WriteLine(array[^1]); // O(1)
+
+            // Comparing dates would have the "Big O" value: O(n^2) 
+            // Might need to change the data structure
+            var array2 = new object[]
+            {
+                new
+                {
+                    tweet = "hi",
+                    date = 2012
+                },
+                new
+                {
+                    tweet = "my",
+                    date = 2014
+                },
+                new
+                {
+                    tweet = "teddy",
+                    date = 2018
+                },
+            };
+
+            // In C# this has "Big O" value: O(1)
+            var length = "asdfasdfasdfadsfa".Length;
+        }
+    }
+}
